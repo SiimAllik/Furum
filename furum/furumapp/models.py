11e5ccdb38fc20f7fac5c2a21e6badcff85b9c02
SiimAllik <siimallik95@gmail.com>
2024-05-20 00:00:38 +0300

    login and register form finished, started file uploads

--- furum/furumapp/models.py
+++ furum/furumapp/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
+import os
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -7,7 +9,11 @@ class Post(models.Model):
     timestamp = models.DateTimeField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     comment_count = models.IntegerField()
-    topic = models.CharField(max_length=100, default="unspecified")
+    topic = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
 
 class Comment(models.Model):
     text = models.CharField(max_length=500)
@@ -15,8 +21,18 @@ class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
+class Topic(models.Model):
+    topic = models.CharField(max_length=1000)
+    slug = models.CharField(max_length=1000)
+    post_count = models.IntegerField()
 
+    def increase_count(self):
+        self.post_count += 1
+        self.save()
 
+    def decrease_count(self):
+        self.post_count -= 1
+        self.save()
 
 
 
--- furum/furumapp/views.py
+++ furum/furumapp/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
 from django.contrib import messages
 from django.contrib.auth import login, authenticate, logout
-from .forms import LoginForm, RegisterForm
-from .models import Post, Comment
+from .forms import LoginForm, RegisterForm, PostForm
+from .models import Post, Comment, Topic
 from django.db.models import Q
 import datetime
+from itertools import chain
+
 def Home_View(request):
-    return render(request, "furumapp/index.html")
+    topics = Topic.objects.all()
+    return render(request, "furumapp/index.html", {"topics" : topics})
 
 def sign_up(request):
     if request.method == 'GET':
@@ -28,23 +31,24 @@ def sign_up(request):
 
 def sign_in(request):
     if request.method == 'GET':
+        form = LoginForm()
         if request.user.is_authenticated:
             return redirect('home')
-        return render(request, 'furumapp/login.html')
+        return render(request, 'furumapp/login.html', {"form" : form})
 
     elif request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-
-        user = authenticate(request, username=username, password=password)
-        if user:
-            login(request, user)
-            messages.success(request, f'Hi {username.title()}, welcome back!')
-            return redirect('home')
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('home')
 
         # form is not valid or user is not authenticated
         messages.error(request, f'Invalid username or password')
-        return render(request, 'furumapp/login.html')
+        return render(request, 'furumapp/login.html', {"form" : form})
 
 def sign_out(request):
     logout(request)
@@ -53,22 +57,28 @@ def sign_out(request):
 
 def Posts_View(request, topic):
     if request.method == 'GET':
+        form = PostForm()
         posts = Post.objects.filter(topic=topic)
-        context = {"posts": posts, "topic": topic}
+        context = {"posts": posts, "topic": topic, "form":form}
         return render(request, "furumapp/postsview.html", context)
     elif request.method == 'POST':
-        post_text = request.POST.get('text')
-        post_title = request.POST.get('title')
-        post = Post(
-            title = post_title,
-            text = post_text,
-            timestamp = datetime.datetime.now(),
-            user=request.user,
-            topic=topic,
-            comment_count=0
-        )
-        post.save()
-        messages.success(request, 'Post successful.')
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post_title = form.cleaned_data['title']
+            post_text = form.cleaned_data['text']
+            image = form.cleaned_data['image']
+            post = Post(
+                title = post_title,
+                text = post_text,
+                timestamp = datetime.datetime.now(),
+                user=request.user,
+                topic=topic,
+                comment_count=0,
+                image=image
+            )
+            post.save()
+            post_topic = Topic.objects.get(slug=topic)
+            post_topic.increase_count()
         return redirect('posts', topic=topic)
 
 def Post_Details(request, post_id):
@@ -108,6 +118,8 @@ def post_delete(request, topic, post_id):
 
     if request.method == 'POST':
         post.delete()
+        post_topic = Topic.objects.get(slug=topic)
+        post_topic.decrease_count()
 
     return redirect('posts', topic=topic)
 
@@ -115,4 +127,8 @@ def search(request):
         if request.method == "GET":
             search_query = request.GET.get('search_query')
             posts = Post.objects.filter(Q(title__icontains=search_query) | Q(text__icontains=search_query))
+            comments = Comment.objects.filter(text__icontains=search_query)
+            for comment in comments:
+                motherpost = Post.objects.filter(id=comment.post.id)
+                posts = posts.union(motherpost)
             return render(request, 'furumapp/searchpost.html', {'query': search_query, 'posts': posts})
